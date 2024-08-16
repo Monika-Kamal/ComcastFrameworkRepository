762ed3364a91a7f6e8ade7ffba0ee40a9b11e1b1
Monika-kamal <monikakamal2000@gmail.com>
2024-08-16 16:32:25 +0530

    org pushed

--- DemoFramework1/src/test/java/pack2/orgTest.java
+++ DemoFramework1/src/test/java/pack2/orgTest.java
@@ -6,6 +6,14 @@ public class orgTest {
 
 	@Test
 	public void createOrgTest() {
+		String Browser = System.getProperty("browser");
+		String url = System.getProperty("url");
+		String username = System.getProperty("username");
+		String password = System.getProperty("password");
+		System.out.println(Browser);
+		System.out.println(url);
+		System.out.println(username);
+		System.out.println(password);
 		System.out.println("execute createOrgTest ");
 	}
 	@Test
