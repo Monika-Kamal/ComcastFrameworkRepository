e61aae536aa024b620b94437b86609b126a682a3
Monika-kamal <monikakamal2000@gmail.com>
2024-08-18 15:57:22 +0530

    pushing new pack3

---
+++ DemoFramework1/src/test/java/pack3/ProductTest.java
@@ -0,0 +1,10 @@
+package pack3;
+
+import org.testng.annotations.Test;
+
+public class ProductTest {
+@Test
+public void createProductTest() {
+	System.out.println("execute createProductTest");
+}
+}
